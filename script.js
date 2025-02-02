917670e0d8990a6b3b3d0102dab1dc86a4eb2feb
Theodora Cristea <cristratheodora@gmail.com>
2025-02-02 18:06:22 +0200

    Verson2

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ links[0].addEventListener('click', changeLink0);
 links[1].addEventListener('click', changeLink1);
 
 function changeLink0() {
-  links[0].classList.toggle('hover-link1');
+  links[0].classList.add('hover-link1');
   
 }
 function changeLink1() {
-  links[1].classList.toggle('hover-link2');
+  links[1].classList.add('hover-link2');
 }
